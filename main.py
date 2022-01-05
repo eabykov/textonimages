bec6be469e120703557c868f225ebd04a69757e9
Yevhenii_Bykov <44170496+eabykov@users.noreply.github.com>
2022-01-05 22:25:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import os
 import imghdr
-from datetime import datetime
 import easyocr
+from os import getenv, walk, path
+from datetime import datetime
 from thefuzz import fuzz
 
-def process_image(image,pattern,reader):
+def process_image(image, pattern, reader):
     result = reader.readtext(image, detail=0, paragraph=True)
     for line in result:
         similarity = fuzz.token_set_ratio(pattern.lower(), line.lower())
@@ -13,19 +13,19 @@ def process_image(image,pattern,reader):
             break
 
 def main():
-    path, pattern, images = os.getenv('EABYKOV_PATH', '/tmp/'), os.getenv('EABYKOV_PATTERN', 'EABYKOV'), []
+    path, pattern, images = getenv('EABYKOV_PATH', '/tmp/'), getenv('EABYKOV_PATTERN', 'EABYKOV'), []
     print(datetime.now(), 'INFO [', main.__name__, '] Will search pattern', pattern, 'in the directory', path)
 
-    for root, directories, files in os.walk(path):
+    for root, directories, files in walk(path):
         for name in files:
-            if imghdr.what(os.path.join(root, name)):
-                images.append(os.path.join(root, name))
+            if imghdr.what(path.join(root, name)):
+                images.append(path.join(root, name))
 
     reader = easyocr.Reader(["ru", "en"])
     print(datetime.now(), 'INFO [', main.__name__, '] Found images:', len(images))
 
     for image in images:
-        process_image(image,pattern,reader)
+        process_image(image, pattern, reader)
 
     print(datetime.now(), 'INFO [', main.__name__, '] End of processing by github.com/eabykov 2022')
 
