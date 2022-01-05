497db89a02104e4b831c614fa9a01d0ee1a212f6
Yevhenii_Bykov <44170496+eabykov@users.noreply.github.com>
2022-01-05 21:33:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ def process_image(image,pattern,reader):
     result = reader.readtext(image, detail=0, paragraph=True)
     for line in result:
         if fuzz.token_set_ratio(pattern.lower(), line.lower()) >= 90:
-            print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'INFO [', process_image.__name__, '] Found the pattern', pattern, 'in the image', image)
+            print(datetime.now(), 'INFO [', process_image.__name__, '] Found the pattern', pattern, 'in the image', image)
             break
 
 def main():
-    print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'INFO [', main.__name__, '] EABYKOV project 2022')
+    print(datetime.now(), 'INFO [', main.__name__, '] EABYKOV project 2022')
     path = os.getenv('EABYKOV_PATH', '/tmp/')
-    print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'INFO [', main.__name__, '] Directory to check:', path)
+    print(datetime.now(), 'INFO [', main.__name__, '] Directory to check:', path)
     pattern = os.getenv('EABYKOV_PATTERN', 'EABYKOV')
-    print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'INFO [', main.__name__, '] Will search pattern:', pattern)
+    print(datetime.now(), 'INFO [', main.__name__, '] Will search pattern:', pattern)
     images = []
 
     for root, directories, files in os.walk(path):
@@ -25,7 +25,7 @@ def main():
                 images.append(os.path.join(root, name))
 
     reader = easyocr.Reader(["ru", "en"])
-    print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'INFO [', main.__name__, '] Found images:', len(images))
+    print(datetime.now(), 'INFO [', main.__name__, '] Found images:', len(images))
 
     for image in images:
         process_image(image,pattern,reader)
