22d76cc2b0fd0ca38f1e94e489604ab12e0f9ef7
Yevhenii_Bykov <44170496+eabykov@users.noreply.github.com>
2022-01-06 16:46:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,10 @@ from thefuzz import fuzz
 
 def process_image(image, pattern, reader):
     try:
-        for line in reader.readtext(image, detail=0, paragraph=True):
-            similarity = fuzz.token_set_ratio(pattern.lower(), line.lower())
-            if similarity >= 90:
-                print(datetime.now(), 'INFO [process_image] Found pattern in the image', image, 'similarity', similarity, '%')
-                break
+        similarity = fuzz.token_set_ratio(pattern.lower(), " ".join(reader.readtext(image, detail=0, paragraph=True)).lower())
+        if similarity >= 90:
+            print(datetime.now(), 'INFO [process_image] Found pattern in the image', image, 'similarity', similarity, '%')
+            break
     except:
         print(datetime.now(), 'ERROR [process_image] Cant process the image ', image)
 
