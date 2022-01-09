b9db1b0c08f6a90c2212ef500e45b56aa94798b5
Yevhenii_Bykov <44170496+eabykov@users.noreply.github.com>
2022-01-09 16:25:43 +0300

    Update easyocr.py

--- easyocr.py
+++ easyocr.py
@@ -2,14 +2,15 @@ import imghdr
 import easyocr
 import os
 import sys
+import platform
 import filecmp
 from datetime import datetime
-from thefuzz import fuzz
+import thefuzz
 
 def process_image(image, pattern, reader):
     try:
         for line in set(reader.readtext(image, detail=0, paragraph=True)):
-            similarity = fuzz.token_set_ratio(pattern.lower(), line.lower())
+            similarity = thefuzz.fuzz.token_set_ratio(pattern.lower(), line.lower())
             if similarity >= 90:
                 print(datetime.now(), 'INFO [process_image] Found pattern in the image', image, 'similarity', similarity, '%')
                 break
@@ -21,6 +22,7 @@ def main():
     if sys.version_info[0] < 3 or os.path.isdir(path) == False:
         sys.exit('ERROR [main] Please use Python 3 and path must be exist directory')
     else:
+        print(datetime.now(), 'INFO [main] Python version', platform.python_version())
         print(datetime.now(), 'INFO [main] Will search pattern', pattern, 'in the directory', path)
 
     print(datetime.now(), 'INFO [list_images] Image list generation has started')
@@ -39,12 +41,14 @@ def main():
         print(datetime.now(), 'INFO [list_images] Found', len(images), 'various images')
 
     try:
+        print(datetime.now(), 'INFO [easyocr_readers] EasyOCR version', easyocr.__version__)
         print(datetime.now(), 'INFO [easyocr_readers] Trying to download the Reader for RU and EN')
         reader = easyocr.Reader(["ru", "en"])
         print(datetime.now(), 'INFO [easyocr_readers] Reader for RU and EN was downloaded')
     except:
         sys.exit('ERROR [easyocr_readers] Cant get Reader for RU and EN')
 
+    print(datetime.now(), 'INFO [process_image] TheFuzz', thefuzz.__version__)
     for image in images:
         process_image(image, pattern, reader)
 
