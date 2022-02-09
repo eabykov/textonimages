3615f910c5d44a7ac7f99d06ad6d7298cf57ac36
Yevhenii Bykov <yevhenii_bykov@epam.com>
2022-02-09 12:29:38 +0300

    black

--- easyocr.py
+++ easyocr.py
@@ -7,29 +7,62 @@ from datetime import datetime
 from thefuzz import fuzz
 import easyocr
 
+
 def process_image(image, pattern, reader):
     try:
-        print(datetime.now(), 'INFO [process_image] The search for pattern on the', image, 'has started')
+        print(
+            datetime.now(),
+            "INFO [process_image] The search for pattern on the",
+            image,
+            "has started",
+        )
         for line in set(reader.readtext(image, detail=0, paragraph=True)):
             similarity = fuzz.token_set_ratio(pattern.lower(), line.lower())
             if similarity >= 90:
-                print(datetime.now(), 'INFO [process_image] Found pattern in the image', image, 'similarity', similarity, '%')
+                print(
+                    datetime.now(),
+                    "INFO [process_image] Found pattern in the image",
+                    image,
+                    "similarity",
+                    similarity,
+                    "%",
+                )
                 return image
                 break
     except:
-        print(datetime.now(), 'ERROR [process_image] Cant search for the pattern on the image ', image)
+        print(
+            datetime.now(),
+            "ERROR [process_image] Cant search for the pattern on the image ",
+            image,
+        )
+
 
 def main():
     faulthandler.enable()
-    path, pattern, images, outputImages = os.getenv('PATH_TO_DIR', '/tmp/'), os.getenv('PATTERN_TO_SEARCH', 'EABYKOV'), [], []
+    path, pattern, images, outputImages = (
+        os.getenv("PATH_TO_DIR", "/tmp/"),
+        os.getenv("PATTERN_TO_SEARCH", "EABYKOV"),
+        [],
+        [],
+    )
     if sys.version_info[0] < 3 or os.path.isdir(path) == False:
-        sys.exit('ERROR [main] Please use Python 3 and path must be exist directory')
+        sys.exit("ERROR [main] Please use Python 3 and path must be exist directory")
     else:
-        print(datetime.now(), 'INFO [main] Python version', ".".join(map(str, sys.version_info[:3])))
-        print(datetime.now(), 'INFO [main] EasyOCR version', easyocr.__version__)
-        print(datetime.now(), 'INFO [main] Will search pattern', pattern, 'in the directory', path)
+        print(
+            datetime.now(),
+            "INFO [main] Python version",
+            ".".join(map(str, sys.version_info[:3])),
+        )
+        print(datetime.now(), "INFO [main] EasyOCR version", easyocr.__version__)
+        print(
+            datetime.now(),
+            "INFO [main] Will search pattern",
+            pattern,
+            "in the directory",
+            path,
+        )
 
-    print(datetime.now(), 'INFO [list_images] Image list generation has started')
+    print(datetime.now(), "INFO [list_images] Image list generation has started")
     for root, directories, files in os.walk(path):
         for name in files:
             imageexist = False
@@ -40,21 +73,35 @@ def main():
             if imghdr.what(os.path.join(root, name)) and imageexist == False:
                 images.append(os.path.join(root, name))
     if images == []:
-        sys.exit('ERROR [list_images] Cant find images in the directory')
+        sys.exit("ERROR [list_images] Cant find images in the directory")
     else:
-        print(datetime.now(), 'INFO [list_images] Found', len(images), 'various images')
+        print(datetime.now(), "INFO [list_images] Found", len(images), "various images")
         images = set(images)
 
     try:
-        print(datetime.now(), 'INFO [easyocr_readers] Trying to download the Reader for RU and EN')
+        print(
+            datetime.now(),
+            "INFO [easyocr_readers] Trying to download the Reader for RU and EN",
+        )
         reader = easyocr.Reader(["ru", "en"])
-        print(datetime.now(), 'INFO [easyocr_readers] Reader for RU and EN was downloaded')
+        print(
+            datetime.now(), "INFO [easyocr_readers] Reader for RU and EN was downloaded"
+        )
     except:
-        sys.exit('ERROR [easyocr_readers] Cant get Reader for RU and EN')
+        sys.exit("ERROR [easyocr_readers] Cant get Reader for RU and EN")
 
-    outputImages = [image for image in images if process_image(image, pattern, reader) == image]
+    outputImages = [
+        image for image in images if process_image(image, pattern, reader) == image
+    ]
     if outputImages != []:
-        print(datetime.now(), 'INFO [main] Found pattern', pattern, 'on the images', outputImages)
+        print(
+            datetime.now(),
+            "INFO [main] Found pattern",
+            pattern,
+            "on the images",
+            outputImages,
+        )
+
 
 if __name__ == "__main__":
     main()
